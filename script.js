a1a23e8f68fc8e473baa929c8d29e373cd38e63d
wbwd <77216609+kmcpherson49@users.noreply.github.com>
2021-02-20 11:58:08 -0700

    add for loop logic

--- script.js
+++ script.js
@@ -1,58 +1,83 @@
 // Assignment code here
+var lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numericChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+//var specialChar = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\", "^",  "_"];
+var specialChar = ["!", "#", "$", "%", "&"];
+
+var userPasswordChoice = [];
+var passLength = ''
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-}
+function generatePassword() {
 
-// length of password //
-var long = function() {
-  window.alert("What is the length of the password?")
-}
+var passLength = prompt("How long would you like your password? Please choose a number between 8 and 128.");
+console.log(passLength);
+if (passLength === "" || passLength === null || isNaN(passLength)) {
+    window.alert("You didn't enter a valid choice, try again!");
+    // use return to call it again and stop the rest of this function from running
+    return generatePassword();
+    }
+ if (passLength < 8 || passLength > 128) {
+     window.alert("You didn't enter a valid choice, try again!");
+    // use return to call it again and stop the rest of this function from running
+     return generatePassword();
+      }
 
 // include lowercase//
 var lowerCase = window.confirm("Do you want your password to contain lowercase letters?");
 // if true use lowercase, else don't use lowercase
 if (lowerCase) {
+    userPasswordChoice.push(lowerChar)
+}
     
-}
-else {
 
-}
+console.log(userPasswordChoice)
 
 
 // include uppercase//
 var upperCase = window.confirm("Do you want your password to contain uppercase letters?");
 if (upperCase) {
-    
+    userPasswordChoice.push(upperChar)
 }
-else {
-    
-}
+console.log(userPasswordChoice)
 
 // include numeric values//
 var numeric = window.confirm("Do you want your password to contain numeric values?");
 if (numeric) {
-    
+    userPasswordChoice.push(numericChar)
 }
-else {
-    
-}
+console.log(userPasswordChoice)
 
 // include lowercase//
 var special = window.confirm("Do you want your password to contain special characters?");
 if (special) {
-    //add characters !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+    userPasswordChoice.push(specialChar)
 }
-else {
-    
+console.log(userPasswordChoice)
+
 }
+
+var newPassword = ""
+
+// for loop userpassword choice array .length add math random inside
+for (var i = 0; i < passLength; i++) {
+   var char = Math.floor(Math.random() * userPasswordChoice)
+   newPassword += userPasswordChoice[char];
+}
+
+// to  display text use text.content
+// Write password to the #password input
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+    passwordText.textContent = newPassword;
+  }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
